f51e9de34d5830652c1ff097e1ecab8bd15dd9da
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-05-02 21:57:23 +0000

    Apply fixes from StyleCI

--- tests/Feature/Category/ReadCategoriesTest.php
+++ tests/Feature/Category/ReadCategoriesTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature\Category;
 use App\Category;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ReadCategoriesTest extends TestCase
