7967c6a3ac7e509b7e13d816438dfd478a27d6cb
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-05-17 11:36:47 +0200

    Wording

--- resources/views/specifications/partials/delete.blade.php
+++ resources/views/specifications/partials/delete.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="deleteCategoryAttributeModal{{ $attribute->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteModal{{ $attribute->id }}" aria-hidden="true">
+<div class="modal fade" id="deleteSpecificationModal{{ $attribute->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteModal{{ $attribute->id }}" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header border-0">
