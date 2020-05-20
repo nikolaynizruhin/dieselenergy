64d7e5bb010d9009eab312bd9320da601ce07b5d
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-05-21 00:16:05 +0200

    Update search input ui

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,7 +9,7 @@
             <form action="{{ route('products.index') }}" method="GET">
                 <div class="form-group">
                     <label for="search" class="sr-only">{{ __('Search') }}</label>
-                    <input type="text" name="search" class="form-control shadow-sm border-0" value="{{ request('search') }}" id="search" aria-describedby="search" placeholder="Search">
+                    <input type="text" name="search" class="form-control shadow-sm" value="{{ request('search') }}" id="search" aria-describedby="search" placeholder="Search">
                 </div>
             </form>
         </div>
