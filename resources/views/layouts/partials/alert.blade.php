0f129e95934c65285311dd4f84fcad76a44711d5
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-05-21 11:22:39 +0200

    Add border to alert

--- resources/views/layouts/partials/alert.blade.php
+++ resources/views/layouts/partials/alert.blade.php
@@ -1,5 +1,5 @@
 @if (session('status'))
-    <div class="alert alert-success alert-dismissible fade show shadow-sm border-0" role="alert">
+    <div class="alert alert-success alert-dismissible fade show shadow-sm" role="alert">
         <div class="d-flex align-items-center">
             <svg class="bi bi-check-circle mr-2" width="1.1em" height="1.1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M15.354 2.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3-3a.5.5 0 11.708-.708L8 9.293l6.646-6.647a.5.5 0 01.708 0z" clip-rule="evenodd"/>
