c164e6c79ad9a3d037fa9934ad3e2104b4575df4
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-05-24 13:29:58 +0200

    Cleanup

--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -5,7 +5,6 @@ namespace Tests\Unit;
 use App\Customer;
 use App\Order;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class OrderTest extends TestCase
