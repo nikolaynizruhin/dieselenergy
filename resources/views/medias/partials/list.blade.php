e7492db625d665e17d490f09defcee7310ca387c
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-06-01 15:27:42 +0200

    Add image link

--- resources/views/medias/partials/list.blade.php
+++ resources/views/medias/partials/list.blade.php
@@ -14,10 +14,18 @@
                 <tr>
                     <th scope="row" class="font-weight-normal">{{ $images->firstItem() + $key }}</th>
                     <td>
-                        <img src="{{ asset($image->path) }}" class="img-thumbnail" alt="{{ $image->path }}" width="64" height="64">
+                        <a href="{{ asset($image->path) }}" target="_blank" class="mr-2">
+                            <img src="{{ asset($image->path) }}" class="img-thumbnail" alt="{{ $image->path }}" width="64" height="64">
+                        </a>
                     </td>
                     <td>{{ basename($image->path) }}</td>
                     <td>
+                        <a href="{{ asset($image->path) }}" target="_blank" class="mr-2">
+                            <svg class="bi bi-eye" width="1.1em" height="1.1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.134 13.134 0 001.66 2.043C4.12 11.332 5.88 12.5 8 12.5c2.12 0 3.879-1.168 5.168-2.457A13.134 13.134 0 0014.828 8a13.133 13.133 0 00-1.66-2.043C11.879 4.668 10.119 3.5 8 3.5c-2.12 0-3.879 1.168-5.168 2.457A13.133 13.133 0 001.172 8z" clip-rule="evenodd"/>
+                                <path fill-rule="evenodd" d="M8 5.5a2.5 2.5 0 100 5 2.5 2.5 0 000-5zM4.5 8a3.5 3.5 0 117 0 3.5 3.5 0 01-7 0z" clip-rule="evenodd"/>
+                            </svg>
+                        </a>
                         <a href="#" data-toggle="modal" data-target="#deleteImageModal{{ $image->id }}">
                             <svg class="bi bi-trash" width="1.1em" height="1.1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>
