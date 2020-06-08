99e3402a5032986b71ad51e98faffab84e635501
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-06-09 00:15:48 +0200

    Add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProductSeeder::class,
             CartSeeder::class,
+            SpecificationSeeder::class,
         ]);
     }
 }
---
+++ database/seeds/SpecificationSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SpecificationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Specification::class, 10)->create();
+    }
+}
