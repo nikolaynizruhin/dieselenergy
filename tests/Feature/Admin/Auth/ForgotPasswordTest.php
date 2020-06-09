ba56fd0fef7ba590211fedf2d6d81c40dab6e5d0
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-06-09 23:31:45 +0200

    Add forgot password test

--- tests/Feature/Admin/Auth/ForgotPasswordTest.php
+++ tests/Feature/Admin/Auth/ForgotPasswordTest.php
@@ -3,8 +3,11 @@
 namespace Tests\Feature\Admin\Auth;
 
 use App\User;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
 class ForgotPasswordTest extends TestCase
@@ -28,6 +31,42 @@ class ForgotPasswordTest extends TestCase
             ->get(route('admin.password.request'))
             ->assertSuccessful()
             ->assertViewIs('admin.auth.passwords.email');
+    }
+
+    /** @test */
+    public function user_receives_email_with_password_reset_link()
+    {
+        Notification::fake();
+
+        $user = factory(User::class)->create();
+
+        $this->post(route('admin.password.email'), ['email' => $user->email]);
+
+        $token = DB::table('password_resets')->where(['email' => $user->email])->first();
+
+        $this->assertNotNull($token);
+
+        Notification::assertSentTo(
+            $user,
+            ResetPassword::class,
+            fn ($notification, $channels) => Hash::check($notification->token, $token->token) === true
+        );
+    }
+
+    /** @test */
+    public function user_does_not_receive_email_when_not_registered()
+    {
+        Notification::fake();
+
+        $user = factory(User::class)->make();
+
+        $this->from(route('admin.password.request'))
+            ->post(route('admin.password.email'), [
+                'email' => $user->email,
+            ])->assertRedirect(route('admin.password.request'))
+            ->assertSessionHasErrors('email');
+
+        Notification::assertNotSentTo($user, ResetPassword::class);
     }
 
     /** @test */
