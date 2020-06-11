a35f26b04bd17abc8252789ec365e081a9a15745
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-06-11 22:03:34 +0200

    Update email translations

--- resources/lang/en/emails.php
+++ resources/lang/en/emails.php
@@ -12,12 +12,17 @@ return [
     |
     */
 
+    'hello' => 'Hello!',
+    'whoops' => 'Whoops!',
+    'regards' => 'Regards',
+
     'reset_password' => [
         'subject' => 'Reset Password Notification',
         'line_1' => 'You are receiving this email because we received a password reset request for your account.',
         'action' => 'Reset Password',
         'line_2' => 'This password reset link will expire in :count minutes.',
         'line_3' => 'If you did not request a password reset, no further action is required.',
+        'footer' => "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".'into your web browser:',
     ]
 
 ];
