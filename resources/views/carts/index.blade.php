db2f99044409087395195b27bb02298e1f1b30ae
Nikolay Nizriukhin <nikolay.nizriukhin@gimmemore.com>
2020-08-22 00:37:44 +0200

    Cleanup UI

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -15,62 +15,64 @@
         <h5 class="text-center text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
         <br>
         <div class="row">
-            <div class="col-8">
+            <div class="col-lg-8">
                 @if ($items->isNotEmpty())
-                    <table class="table border">
-                        <tbody>
-                        @foreach($items as $key => $item)
-                            <tr>
-                                <td>
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <img src="{{ asset('/storage/images/cXeaYwhazLxNFIyz7dqcCzrXNuaGrbSYKYpPbavq.jpeg') }}" width="100" alt="...">
-                                        </div>
-                                        <div class="col d-flex align-items-center">
-                                            <div class="d-flex flex-column">
-                                                <span class="font-weight-bold">{{ $item->name }}</span>
-                                                <span class="text-muted">{{ $item->category }}</span>
+                    <div class="table-responsive">
+                        <table class="table border">
+                            <tbody>
+                            @foreach($items as $key => $item)
+                                <tr>
+                                    <td>
+                                        <div class="row">
+                                            <div class="col-4">
+                                                <img src="{{ asset('/storage/images/cXeaYwhazLxNFIyz7dqcCzrXNuaGrbSYKYpPbavq.jpeg') }}" width="100" alt="...">
+                                            </div>
+                                            <div class="col d-flex align-items-center">
+                                                <div class="d-flex flex-column">
+                                                    <span class="font-weight-bold">{{ $item->name }}</span>
+                                                    <span class="text-muted">{{ $item->category }}</span>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </td>
-                                <td class="align-middle @if ($loop->first) border-top-0 @endif">@usd($item->price)</td>
-                                <td style="width: 12%" class="align-middle @if ($loop->first) border-top-0 @endif">
-                                    <form action="{{ route('carts.update', $key) }}" method="POST">
-                                        @csrf
-                                        @method('PUT')
-                                        <div class="form-group mb-0">
-                                            <label for="quantity" class="sr-only">Amount</label>
-                                            <input type="number" name="quantity" onchange="this.form.submit()" value="{{ $item->quantity }}" min="1" class="form-control" id="quantity" aria-describedby="quantityHelp">
-                                        </div>
-                                    </form>
-                                </td>
-                                <td class="align-middle @if ($loop->first) border-top-0 @endif">@usd($item->total())</td>
-                                <td class="align-middle @if ($loop->first) border-top-0 @endif">
-                                    <form action="{{ route('carts.destroy', $key) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-link">
-                                            <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
-                                                <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
-                                            </svg>
-                                        </button>
-                                    </form>
-                                </td>
+                                    </td>
+                                    <td class="align-middle @if ($loop->first) border-top-0 @endif">@usd($item->price)</td>
+                                    <td style="width: 12%" class="align-middle @if ($loop->first) border-top-0 @endif">
+                                        <form action="{{ route('carts.update', $key) }}" method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <div class="form-group mb-0">
+                                                <label for="quantity" class="sr-only">Amount</label>
+                                                <input type="number" name="quantity" onchange="this.form.submit()" value="{{ $item->quantity }}" min="1" class="form-control" id="quantity" aria-describedby="quantityHelp">
+                                            </div>
+                                        </form>
+                                    </td>
+                                    <td class="align-middle @if ($loop->first) border-top-0 @endif">@usd($item->total())</td>
+                                    <td class="align-middle @if ($loop->first) border-top-0 @endif">
+                                        <form action="{{ route('carts.destroy', $key) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-link">
+                                                <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
+                                                    <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
+                                                </svg>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            <tr>
+                                <td colspan="3">Total:</td>
+                                <td colspan="2">@usd($total)</td>
                             </tr>
-                        @endforeach
-                        <tr>
-                            <td colspan="3">Total:</td>
-                            <td colspan="2">@usd($total)</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 @else
                     <p>Cart is empty</p>
                 @endif
             </div>
-            <div class="col-4">
+            <div class="col-lg-4">
                 @if ($items->isNotEmpty())
                     <div class="card">
                         <div class="card-body">
