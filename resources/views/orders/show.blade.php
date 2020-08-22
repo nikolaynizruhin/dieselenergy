30de23fc786b578976599f46edbb0d8f6b9baddf
Nikolay Nizriukhin <nikolay.nizriukhin@gimmemore.com>
2020-08-22 23:07:04 +0200

    Update UI

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -12,13 +12,11 @@
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-6 text-center">
-                <p>
-                    <svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-check-circle text-success" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                        <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
-                    </svg>
-                </p>
+            <div class="col-6 py-5 text-center">
+                <svg width="4em" height="4em" viewBox="0 0 16 16" class="bi bi-check-circle text-success mb-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+                </svg>
                 <h1>Congratulations.<br>Your order is accepted.</h1>
                 <p class="text-muted">Thank you for your order! Your order is being processed and will be completed within 3-6 hours. You will receive an email confirmation when your order is completed.</p>
                 <p class="text-muted">Your order number: QW341JK</p>
