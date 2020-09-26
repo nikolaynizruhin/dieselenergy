bd2af96563add286e91f708d82fc0634253d001a
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-09-26 14:36:10 +0200

    Update posts UI

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,15 +2,23 @@
 
 @section('content')
     <div class="container py-5">
+        <div class="col d-flex justify-content-center flex-column mb-5">
+            <div class="text-center">
+                <h2>From The Blog</h2>
+                <h5 class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h5>
+            </div>
+        </div>
         @forelse ($posts->chunk(3) as $chunkedPosts)
             <div class="card-deck">
                 @foreach ($chunkedPosts as $post)
-                    <div class="card">
+                    <div class="card shadow">
                         <img src="{{ asset('storage/'.$post->image->path) }}" class="card-img-top" alt="{{ $post->title }}">
                         <div class="card-body">
                             <h5 class="card-title">{{ $post->title }}</h5>
                             <p class="card-text">{{ $post->excerpt }}</p>
-                            <p class="card-text"><small class="text-muted">{{ $post->created_at }}</small></p>
+                        </div>
+                        <div class="card-footer bg-white border-top-0">
+                            <small class="text-muted">{{ $post->created_at }}</small>
                         </div>
                     </div>
                 @endforeach
@@ -18,5 +26,10 @@
         @empty
             <p>No posts matched the given criteria.</p>
         @endforelse
+        <div class="row">
+            <div class="col offset-md-3 offset-lg-2">
+                {{ $posts->links('layouts.partials.pagination') }}
+            </div>
+        </div>
     </div>
 @endsection
