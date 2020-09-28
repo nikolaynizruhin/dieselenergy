6702e68091642caf9635cafb405c094fd3c8a4be
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-09-28 10:04:14 +0200

    Update post UI

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
     <div class="container py-5">
-        <h1>{{ $post->title }}</h1>
-        <hr>
+        <div class="row mt-n3 mb-4 text-gray-500">
+            <div class="col letter-spacing d-flex align-items-center">
+                Posts
+                @include('layouts.partials.icon', ['name' => 'chevron-right', 'classes' => 'mx-2', 'width' => '0.9em', 'height' => '0.9em'])
+                {{ $post->title }}
+            </div>
+        </div>
+        <h3>{{ $post->title }}</h3>
         @markdown($post->body)
         <small class="text-muted">{{ $post->created_at }}</small>
     </div>
