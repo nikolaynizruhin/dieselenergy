755baf3681fd6b86e9e95d000cd40804b4398e2b
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-10-07 00:12:18 +0200

    Add dot patterns

--- resources/views/categories/products/index.blade.php
+++ resources/views/categories/products/index.blade.php
@@ -1,102 +1,106 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container py-5">
-        <div class="row mt-n3 mb-4 text-gray-500">
-            <div class="col letter-spacing d-flex align-items-center">
-                Магазин
-                @include('layouts.partials.icon', ['name' => 'chevron-right', 'classes' => 'mx-2', 'width' => '0.9em', 'height' => '0.9em'])
-                {{ $category->name }}
+    <section class="position-relative py-5">
+        <div class="dots mr-sm-5 mt-6 top-0 right-0 height-72 width-48 position-absolute"></div>
+
+        <div class="container">
+            <div class="row mt-n3 mb-4 text-gray-500">
+                <div class="col letter-spacing d-flex align-items-center">
+                    Магазин
+                    @include('layouts.partials.icon', ['name' => 'chevron-right', 'classes' => 'mx-2', 'width' => '0.9em', 'height' => '0.9em'])
+                    {{ $category->name }}
+                </div>
+                <div class="col text-right">
+                    Всього: {{ $products->total() }}
+                </div>
             </div>
-            <div class="col text-right">
-                Всього: {{ $products->total() }}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 col-md-3 col-lg-2">
-                <form>
-                    <p class="mb-1 font-weight-bold text-uppercase letter-spacing text-gray-500">
-                        <small>Пошук</small>
-                    </p>
+            <div class="row">
+                <div class="col-12 col-md-3 col-lg-2">
+                    <form>
+                        <p class="mb-1 font-weight-bold text-uppercase letter-spacing text-gray-500">
+                            <small>Пошук</small>
+                        </p>
 
-                    <div class="form-group">
-                        <label for="inputSearch" class="sr-only">Пошук</label>
-                        <input type="text" name="search" value="{{ request('search') }}" class="form-control form-control-sm" id="inputSearch" aria-describedby="emailHelp" placeholder="Введіть...">
-                    </div>
+                        <div class="form-group">
+                            <label for="inputSearch" class="sr-only">Пошук</label>
+                            <input type="text" name="search" value="{{ request('search') }}" class="form-control form-control-sm" id="inputSearch" aria-describedby="emailHelp" placeholder="Введіть...">
+                        </div>
 
-                    <p class="mb-1 font-weight-bold text-uppercase letter-spacing text-gray-500">
-                        <small>Сортувати</small>
-                    </p>
+                        <p class="mb-1 font-weight-bold text-uppercase letter-spacing text-gray-500">
+                            <small>Сортувати</small>
+                        </p>
 
-                    <div class="form-group">
-                        <label for="selectSort" class="sr-only">Sort</label>
-                        <select class="form-control form-control-sm" name="sort" id="selectSort">
-                            <option value="name" @if (request('sort') === 'name') selected @endif>Назва (А - Я)</option>
-                            <option value="-name" @if (request('sort') === '-name') selected @endif>Назва (Я - А)</option>
-                        </select>
-                    </div>
+                        <div class="form-group">
+                            <label for="selectSort" class="sr-only">Sort</label>
+                            <select class="form-control form-control-sm" name="sort" id="selectSort">
+                                <option value="name" @if (request('sort') === 'name') selected @endif>Назва (А - Я)</option>
+                                <option value="-name" @if (request('sort') === '-name') selected @endif>Назва (Я - А)</option>
+                            </select>
+                        </div>
 
-                    @includeIf('categories.products.partials.filters.'.$category->slug)
+                        @includeIf('categories.products.partials.filters.'.$category->slug)
 
-                    <button type="submit" class="btn mt-3 btn-outline-primary btn-block">
-                        Filter
-                    </button>
-                </form>
-            </div>
-            <div class="col-12 col-md-9 col-lg-10">
-                @forelse ($products->chunk(3) as $chunkedProducts)
-                    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-                        @foreach ($chunkedProducts as $product)
-                            <div class="col mb-4">
-                                <div class="card">
-                                    <img src="{{ asset('storage/'.$product->defaultImage()->path) }}" class="card-img-top" alt="...">
-                                    <div class="card-body mb-n3">
-                                        <h6 class="card-title">
-                                            <a href="{{ route('products.show', $product) }}" class="text-decoration-none text-body">
-                                                {{ $product->name }}
-                                            </a>
-                                        </h6>
-                                        <h6 class="card-subtitle mb-2 text-muted">{{ $product->category->name }}</h6>
-                                        <h5 class="card-title">@usd($product->price)</h5>
-                                    </div>
-                                    @if ($product->attributes->isNotEmpty())
-                                        <div class="card-body bg-light text-muted">
-                                            @foreach ($product->attributes as $attribute)
-                                                <div class="row mb-1">
-                                                    <div class="col letter-spacing text-gray-500 text-uppercase">
-                                                        <small>{{ $attribute->name }}</small>
-                                                    </div>
-                                                    <div class="col text-secondary">
-                                                        <small>{{ $attribute->pivot->value . $attribute->measure }}</small>
-                                                    </div>
-                                                </div>
-                                            @endforeach
+                        <button type="submit" class="btn mt-3 btn-outline-primary btn-block">
+                            Filter
+                        </button>
+                    </form>
+                </div>
+                <div class="col-12 col-md-9 col-lg-10">
+                    @forelse ($products->chunk(3) as $chunkedProducts)
+                        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                            @foreach ($chunkedProducts as $product)
+                                <div class="col mb-4">
+                                    <div class="card">
+                                        <img src="{{ asset('storage/'.$product->defaultImage()->path) }}" class="card-img-top" alt="...">
+                                        <div class="card-body mb-n3">
+                                            <h6 class="card-title">
+                                                <a href="{{ route('products.show', $product) }}" class="text-decoration-none text-body">
+                                                    {{ $product->name }}
+                                                </a>
+                                            </h6>
+                                            <h6 class="card-subtitle mb-2 text-muted">{{ $product->category->name }}</h6>
+                                            <h5 class="card-title">@usd($product->price)</h5>
                                         </div>
-                                    @endif
-                                    <div class="card-body">
-                                        <form action="{{ route('carts.store') }}" method="POST">
-                                            @csrf
-                                            <input type="hidden" name="product_id" value="{{ $product->id }}">
-                                            <input type="hidden" name="quantity" value="1">
-                                            <button type="submit" class="btn btn-outline-secondary btn-block">
-                                                Add to Cart
-                                            </button>
-                                        </form>
+                                        @if ($product->attributes->isNotEmpty())
+                                            <div class="card-body bg-light text-muted">
+                                                @foreach ($product->attributes as $attribute)
+                                                    <div class="row mb-1">
+                                                        <div class="col letter-spacing text-gray-500 text-uppercase">
+                                                            <small>{{ $attribute->name }}</small>
+                                                        </div>
+                                                        <div class="col text-secondary">
+                                                            <small>{{ $attribute->pivot->value . $attribute->measure }}</small>
+                                                        </div>
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        @endif
+                                        <div class="card-body">
+                                            <form action="{{ route('carts.store') }}" method="POST">
+                                                @csrf
+                                                <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                                <input type="hidden" name="quantity" value="1">
+                                                <button type="submit" class="btn btn-outline-secondary btn-block">
+                                                    Add to Cart
+                                                </button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endforeach
-                    </div>
-                @empty
-                    <p>No products matched the given criteria.</p>
-                @endforelse
+                            @endforeach
+                        </div>
+                    @empty
+                        <p>No products matched the given criteria.</p>
+                    @endforelse
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col offset-md-3 offset-lg-2">
+                    {{ $products->withQueryString()->links('layouts.partials.pagination') }}
+                </div>
             </div>
         </div>
-        <br>
-        <div class="row">
-            <div class="col offset-md-3 offset-lg-2">
-                {{ $products->withQueryString()->links('layouts.partials.pagination') }}
-            </div>
-        </div>
-    </div>
+    </section>
 @endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,104 +1,109 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container py-5">
-        <div class="row mt-n3 mb-4 text-gray-500">
-            <div class="col letter-spacing d-flex align-items-center">
-                Магазин
-                @include('layouts.partials.icon', ['name' => 'chevron-right', 'classes' => 'mx-2', 'width' => '0.9em', 'height' => '0.9em'])
-                {{ $product->category->name }}
-                @include('layouts.partials.icon', ['name' => 'chevron-right', 'classes' => 'mx-2', 'width' => '0.9em', 'height' => '0.9em'])
-                {{ $product->name }}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 col-md">
-                <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        @foreach($product->images as $image)
-                            <div class="carousel-item active">
-                                <img src="{{ asset('/storage/'.$image->path) }}" class="d-block w-100" alt="{{ $product->name }}">
-                            </div>
-                        @endforeach
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+    <section class="position-relative py-5">
+        <div class="dots mr-sm-5 mt-5 top-0 right-0 height-72 width-48 position-absolute"></div>
+        <div class="dots ml-sm-5 mb-4 bottom-0 left-0 height-72 width-48 position-absolute"></div>
+
+        <div class="container">
+            <div class="row mt-n3 mb-4 text-gray-500">
+                <div class="col letter-spacing d-flex align-items-center">
+                    Магазин
+                    @include('layouts.partials.icon', ['name' => 'chevron-right', 'classes' => 'mx-2', 'width' => '0.9em', 'height' => '0.9em'])
+                    {{ $product->category->name }}
+                    @include('layouts.partials.icon', ['name' => 'chevron-right', 'classes' => 'mx-2', 'width' => '0.9em', 'height' => '0.9em'])
+                    {{ $product->name }}
                 </div>
             </div>
-            <div class="col-12 col-md">
-                <h5 class="font-weight-bold mb-3">
-                    {{ $product->name }}
-                    <br>
-                    <small class="text-muted">{{ $product->category->name }}</small>
-                </h5>
-                <h5 class="text-primary">@usd($product->price)</h5>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur assumenda consectetur, debitis dicta dolorum earum eius eos inventore libero maxime molestiae nostrum obcaecati odit possimus quo recusandae sed totam.
-                </p>
-                <form action="{{ route('carts.store') }}" method="POST">
-                    @csrf
-                    <div class="row">
-                        <div class="col-2 col-md-3 col-lg-2">
-                            <div class="form-group">
-                                <label for="inputAmount" class="sr-only">Amount</label>
-                                <input type="number" name="quantity" value="1" min="1" max="99" class="form-control" id="inputAmount" aria-describedby="amountHelp">
-                                <input type="hidden" name="product_id" value="{{ $product->id }}">
+            <div class="row">
+                <div class="col-12 col-md">
+                    <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselIndicators" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            @foreach($product->images as $image)
+                                <div class="carousel-item active">
+                                    <img src="{{ asset('/storage/'.$image->path) }}" class="d-block w-100" alt="{{ $product->name }}">
+                                </div>
+                            @endforeach
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-12 col-md">
+                    <h5 class="font-weight-bold mb-3">
+                        {{ $product->name }}
+                        <br>
+                        <small class="text-muted">{{ $product->category->name }}</small>
+                    </h5>
+                    <h5 class="text-primary">@usd($product->price)</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur assumenda consectetur, debitis dicta dolorum earum eius eos inventore libero maxime molestiae nostrum obcaecati odit possimus quo recusandae sed totam.
+                    </p>
+                    <form action="{{ route('carts.store') }}" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="col-2 col-md-3 col-lg-2">
+                                <div class="form-group">
+                                    <label for="inputAmount" class="sr-only">Amount</label>
+                                    <input type="number" name="quantity" value="1" min="1" max="99" class="form-control" id="inputAmount" aria-describedby="amountHelp">
+                                    <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <button type="submit" class="btn btn-primary">Add to Cart</button>
                             </div>
                         </div>
-                        <div class="col">
-                            <button type="submit" class="btn btn-primary">Add to Cart</button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Опис</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Характеристики</a>
-                    </li>
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                        <br>
-                        @markdown($product->description)
-                    </div>
-                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                        <br>
-                        <table class="table table-bordered">
-                            <thead>
+            <div class="row">
+                <div class="col">
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Опис</a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Характеристики</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <br>
+                            @markdown($product->description)
+                        </div>
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <br>
+                            <table class="table table-bordered">
+                                <thead>
                                 <tr>
                                     <th scope="col">Назва</th>
                                     <th scope="col">Значення</th>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($product->attributes as $attribute)
-                                    <tr>
-                                        <th scope="row">{{ $attribute->field }}</th>
-                                        <td>{{ $attribute->pivot->value }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach($product->attributes as $attribute)
+                                        <tr>
+                                            <th scope="row">{{ $attribute->field }}</th>
+                                            <td>{{ $attribute->pivot->value }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
