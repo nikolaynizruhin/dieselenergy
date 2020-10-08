666ad2b9a9b9d5afd5430c6095670e52f0c0ff98
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-10-09 00:08:00 +0200

    Add teal color

--- resources/views/admin/layouts/partials/alert.blade.php
+++ resources/views/admin/layouts/partials/alert.blade.php
@@ -1,7 +1,7 @@
 @if (session('status'))
     <div class="alert alert-success alert-dismissible fade show shadow-sm" role="alert">
         <div class="d-flex align-items-center">
-            @include('layouts.partials.icon', ['name' => 'check-circle', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
+            @include('layouts.partials.icon', ['name' => 'check-circle-fill', 'classes' => 'mr-3 text-teal', 'width' => '1.1em', 'height' => '1.1em'])
             {{ session('status') }}
         </div>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
