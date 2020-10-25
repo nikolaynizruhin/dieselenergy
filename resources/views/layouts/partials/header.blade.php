5739ad0717bdaea871086fa68e59b1295f4ef18f
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-10-25 21:37:28 +0100

    Add cart indicator

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -33,7 +33,14 @@
                     <a class="nav-link" href="{{ route('home').'#numbers' }}">Про нас</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('carts.index') }}">Кошик</a>
+                    <a class="nav-link position-relative" href="{{ route('carts.index') }}">
+                        Кошик
+                        @if(\Facades\App\Cart\Cart::items()->isNotEmpty())
+                            <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-dot text-primary position-absolute top-0 ml-n2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                            </svg>
+                        @endif
+                    </a>
                 </li>
             </ul>
         </div>
