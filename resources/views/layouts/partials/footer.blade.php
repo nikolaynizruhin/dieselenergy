f4de364d477e32d5412755e5f24942a49f8136c4
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-10-26 22:22:43 +0100

    Update footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,50 +1,47 @@
 <footer class="bg-dark mt-auto py-5">
     <div class="container">
-
         <div class="row mb-5">
-            <div class="col-12 col-sm-4">
-                <a class="navbar-brand d-flex text-gray-400 mb-2" href="{{ route('home') }}">
-                    @include('layouts.partials.icon', ['name' => 'intersect', 'classes' => 'mr-3 text-primary', 'width' => '1.5em', 'height' => '1.5em'])
-                    {{ config('app.name') }}
+            <div class="col-12 col-sm-3">
+                <a class="text-gray-400" href="{{ route('home') }}">
+                    <h5 class="d-flex align-items-center">
+                        @include('layouts.partials.icon', ['name' => 'intersect', 'classes' => 'mr-3 text-primary', 'width' => '1.5em', 'height' => '1.5em'])
+                        {{ config('app.name') }}
+                    </h5>
                 </a>
                 <p class="text-gray-400">Надійні рішення для постачання<br>електроенергії</p>
             </div>
-            <div class="col-12 col-sm-8">
-                <div class="row">
-                    <div class="col-12 col-sm-4">
-                        <span class="d-block text-gray-500 mb-3">ТОВАРИ</span>
-                        @foreach(\App\Models\Category::all() as $category)
-                            <a class="d-block text-gray-300 mb-2" href="{{ route('categories.products.index', $category) }}">{{ $category->name }}</a>
-                        @endforeach
-                    </div>
-                    <div class="col-12 col-sm-4">
-                        <span class="d-block text-gray-500 mb-3">КОМПАНІЯ</span>
-                        <a class="d-block text-gray-300 mb-2" href="{{ route('home').'#services' }}">Послуги</a>
-                        <a class="d-block text-gray-300 mb-2" href="{{ route('home').'#contact' }}">Контакти</a>
-                        <a class="d-block text-gray-300 mb-2" href="{{ route('home').'#numbers' }}">Про нас</a>
-                    </div>
-                    <div class="col-12 col-sm-4">
-                        <span class="d-block text-gray-500 mb-3">КОНТАКТИ</span>
-                        <p class="mb-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone text-gray-400 mr-2">
-                                <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-                            </svg>
-                            <span class="text-gray-400">+38 (063) 168 42 45</span>
-                        </p>
-                        <p class="mb-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail text-gray-400 mr-2">
-                                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline>
-                            </svg>
-                            <span class="text-gray-400">diesel.energy@gmail.com</span>
-                        </p>
-                        <p class="mb-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook text-gray-400 mr-2">
-                                <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                            </svg>
-                            <span class="text-gray-400">Diesel Energy</span>
-                        </p>
-                    </div>
-                </div>
+            <div class="col-12 col-sm-3">
+                <span class="d-block text-gray-500 mb-3">ТОВАРИ</span>
+                @foreach(\App\Models\Category::all() as $category)
+                    <a class="d-block text-gray-300 mb-2" href="{{ route('categories.products.index', $category) }}">{{ $category->name }}</a>
+                @endforeach
+            </div>
+            <div class="col-12 col-sm-3">
+                <span class="d-block text-gray-500 mb-3">КОМПАНІЯ</span>
+                <a class="d-block text-gray-300 mb-2" href="{{ route('home').'#services' }}">Послуги</a>
+                <a class="d-block text-gray-300 mb-2" href="{{ route('posts.index') }}">Блог</a>
+                <a class="d-block text-gray-300 mb-2" href="{{ route('home').'#numbers' }}">Про нас</a>
+            </div>
+            <div class="col-12 col-sm-3">
+                <span class="d-block text-gray-500 mb-3">КОНТАКТИ</span>
+                <a href="tel:+380977997542" class="d-block mb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone text-gray-400 mr-2">
+                        <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                    </svg>
+                    <span class="text-gray-400">+38 (097) 799 75 42</span>
+                </a>
+                <a href="mailto:diesel.energy.llc@gmail.com" class="d-block mb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail text-gray-400 mr-2">
+                        <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline>
+                    </svg>
+                    <span class="text-gray-400">diesel.energy.llc@gmail.com</span>
+                </a>
+                <a href="https://www.facebook.com/Diesel-Energy-473907976127278/" class="d-block mb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook text-gray-400 mr-2">
+                        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+                    </svg>
+                    <span class="text-gray-400">Diesel Energy</span>
+                </a>
             </div>
         </div>
 
