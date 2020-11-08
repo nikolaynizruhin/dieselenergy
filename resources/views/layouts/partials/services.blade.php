5d71af78bb2198f83b22a6a38209e9af56afa46a
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-11-08 21:02:02 +0100

    Update services

--- resources/views/layouts/partials/services.blade.php
+++ resources/views/layouts/partials/services.blade.php
@@ -4,33 +4,33 @@
             <div class="col-12 col-md mb-5 mb-md-0">
                 <div class="row">
                     <div class="col-3">
-                        @include('layouts.partials.icon', ['name' => 'bag-check', 'width' => '3em', 'height' => '3em', 'classes' => 'text-secondary'])
+                        @include('layouts.partials.icon', ['name' => 'truck', 'width' => '3em', 'height' => '3em', 'classes' => 'text-secondary'])
                     </div>
                     <div class="col">
-                        <h5>FREE SHIPPING</h5>
-                        <p class="text-muted">Free shipping over $300</p>
+                        <h5>БЕЗКОШТОВНА ДОСТАВКА</h5>
+                        <p class="text-muted">Безкоштовна доставка по Києву та Київській області</p>
                     </div>
                 </div>
             </div>
             <div class="col-12 col-md mb-5 mb-md-0">
                 <div class="row">
                     <div class="col-3">
-                        @include('layouts.partials.icon', ['name' => 'cash-stack', 'width' => '3em', 'height' => '3em', 'classes' => 'text-secondary'])
+                        @include('layouts.partials.icon', ['name' => 'shield-check', 'width' => '3em', 'height' => '3em', 'classes' => 'text-secondary'])
                     </div>
                     <div class="col">
-                        <h5>MONEY BACK</h5>
-                        <p class="text-muted">30 days money back guarantee</p>
+                        <h5>ГАРАНТІЯ</h5>
+                        <p class="text-muted">Гарантія протягом 1 року</p>
                     </div>
                 </div>
             </div>
             <div class="col-12 col-md">
                 <div class="row">
                     <div class="col-3">
-                        @include('layouts.partials.icon', ['name' => 'headset', 'width' => '3em', 'height' => '3em', 'classes' => 'text-secondary'])
+                        @include('layouts.partials.icon', ['name' => 'tools', 'width' => '3em', 'height' => '3em', 'classes' => 'text-secondary'])
                     </div>
                     <div class="col">
-                        <h5>SUPPORT</h5>
-                        <p class="text-muted">24/7 call center support</p>
+                        <h5>ТЕХНІЧНА ПІДТРИМКА</h5>
+                        <p class="text-muted">Технічна підтримка протягом 1 року</p>
                     </div>
                 </div>
             </div>
