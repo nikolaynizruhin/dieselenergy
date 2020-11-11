7f3f2fb710eb0eab6c821e08a5f5c11c07b40310
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-11-11 23:53:45 +0100

    Update UI

--- resources/views/layouts/partials/services.blade.php
+++ resources/views/layouts/partials/services.blade.php
@@ -4,10 +4,10 @@
             <div class="col-12 col-md mb-5 mb-md-0">
                 <div class="row">
                     <div class="col-3">
-                        @include('layouts.partials.icon', ['name' => 'truck', 'width' => '3em', 'height' => '3em', 'classes' => 'text-secondary'])
+                        @include('layouts.partials.icon', ['name' => 'truck', 'width' => '2.5em', 'height' => '2.5em', 'classes' => 'text-secondary'])
                     </div>
                     <div class="col">
-                        <h5>БЕЗКОШТОВНА ДОСТАВКА</h5>
+                        БЕЗКОШТОВНА ДОСТАВКА
                         <p class="text-muted">Безкоштовна доставка по Києву та Київській області</p>
                     </div>
                 </div>
@@ -15,10 +15,10 @@
             <div class="col-12 col-md mb-5 mb-md-0">
                 <div class="row">
                     <div class="col-3">
-                        @include('layouts.partials.icon', ['name' => 'shield-check', 'width' => '3em', 'height' => '3em', 'classes' => 'text-secondary'])
+                        @include('layouts.partials.icon', ['name' => 'shield-check', 'width' => '2.5em', 'height' => '2.5em', 'classes' => 'text-secondary'])
                     </div>
                     <div class="col">
-                        <h5>ГАРАНТІЯ</h5>
+                        ГАРАНТІЯ
                         <p class="text-muted">Гарантія протягом 1 року</p>
                     </div>
                 </div>
@@ -26,10 +26,10 @@
             <div class="col-12 col-md">
                 <div class="row">
                     <div class="col-3">
-                        @include('layouts.partials.icon', ['name' => 'tools', 'width' => '3em', 'height' => '3em', 'classes' => 'text-secondary'])
+                        @include('layouts.partials.icon', ['name' => 'tools', 'width' => '2.5em', 'height' => '2.5em', 'classes' => 'text-secondary'])
                     </div>
                     <div class="col">
-                        <h5>ТЕХНІЧНА ПІДТРИМКА</h5>
+                        ТЕХНІЧНА ПІДТРИМКА
                         <p class="text-muted">Технічна підтримка протягом 1 року</p>
                     </div>
                 </div>
