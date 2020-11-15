9da54081d6200e2193db999cd44e562abd766adf
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-11-15 20:18:52 +0100

    Update product layout

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -70,8 +70,8 @@
                     </p>
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
+            <div class="row justify-content-center">
+                <div class="col col-md-10 col-lg-10">
                     <ul class="nav nav-tabs" id="myTab" role="tablist">
                         <li class="nav-item" role="presentation">
                             <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Опис</a>
@@ -81,8 +81,7 @@
                         </li>
                     </ul>
                     <div class="tab-content" id="myTabContent">
-                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                            <br>
+                        <div class="tab-pane fade show active text-justify pt-4" id="home" role="tabpanel" aria-labelledby="home-tab">
                             @markdown($product->description)
                         </div>
                         <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
