87608847295801f3f57f922e545423c5e4dea531
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-11-19 00:39:29 +0100

    Update checkboxes

--- resources/views/categories/products/partials/filters/ats.blade.php
+++ resources/views/categories/products/partials/filters/ats.blade.php
@@ -2,16 +2,16 @@
     <small>Напруга</small>
 </p>
 
-<div class="form-check">
-    <input name="filter[3][]" value="230" class="form-check-input" type="checkbox" id="defaultCheck1" @if(in_array('230', request('filter.3', []))) checked @endif>
-    <label class="form-check-label text-secondary" for="defaultCheck1">
+<div class="custom-control custom-checkbox">
+    <input name="filter[3][]" value="230" type="checkbox" class="custom-control-input" id="customCheck1" @if(in_array('230', request('filter.3', []))) checked @endif>
+    <label class="custom-control-label text-secondary" for="customCheck4">
         <small>230</small>
     </label>
 </div>
 
-<div class="form-check">
-    <input name="filter[3][]" value="400" class="form-check-input" type="checkbox" id="defaultCheck2" @if(in_array('400', request('filter.3', []))) checked @endif>
-    <label class="form-check-label text-secondary" for="defaultCheck2">
+<div class="custom-control custom-checkbox">
+    <input name="filter[3][]" value="400" type="checkbox" class="custom-control-input" id="customCheck2" @if(in_array('400', request('filter.3', []))) checked @endif>
+    <label class="custom-control-label text-secondary" for="customCheck2">
         <small>400</small>
     </label>
 </div>
@@ -20,9 +20,9 @@
     <small>Потужність</small>
 </p>
 
-<div class="form-check">
-    <input name="filter[23][]" class="form-check-input" type="checkbox" value="10" id="defaultCheck1" @if(in_array('10', request('filter.23', []))) checked @endif>
-    <label class="form-check-label text-secondary" for="defaultCheck1">
+<div class="custom-control custom-checkbox">
+    <input name="filter[23][]" value="10" type="checkbox" class="custom-control-input" id="customCheck3" @if(in_array('10', request('filter.23', []))) checked @endif>
+    <label class="custom-control-label text-secondary" for="customCheck2">
         <small>10</small>
     </label>
 </div>
--- resources/views/categories/products/partials/filters/waterpumps.blade.php
+++ resources/views/categories/products/partials/filters/waterpumps.blade.php
@@ -2,35 +2,17 @@
     <small>Об'єм двигуна</small>
 </p>
 
-<div class="form-check">
-    <input name="filter[32][]" value="212" class="form-check-input" type="checkbox" id="defaultCheck1" @if(in_array('212', request('filter.32', []))) checked @endif>
-    <label class="form-check-label text-secondary" for="defaultCheck1">
+<div class="custom-control custom-checkbox">
+    <input name="filter[32][]" value="212" type="checkbox" class="custom-control-input" id="customCheck1" @if(in_array('212', request('filter.32', []))) checked @endif>
+    <label class="custom-control-label text-secondary" for="customCheck1">
         <small>212</small>
     </label>
 </div>
 
-<div class="form-check">
-    <input name="filter[32][]" value="270" class="form-check-input" type="checkbox" id="defaultCheck2" @if(in_array('270', request('filter.32', []))) checked @endif>
-    <label class="form-check-label text-secondary" for="defaultCheck2">
+<div class="custom-control custom-checkbox">
+    <input name="filter[32][]" value="270" type="checkbox" class="custom-control-input" id="customCheck2" @if(in_array('270', request('filter.32', []))) checked @endif>
+    <label class="custom-control-label text-secondary" for="customCheck2">
         <small>270</small>
-    </label>
-</div>
-
-<p class="mb-1 mt-3 font-weight-bold text-uppercase letter-spacing text-gray-500">
-    <small>Вихідна потужність двигуна</small>
-</p>
-
-<div class="form-check">
-    <input name="filter[12][]" class="form-check-input" type="checkbox" value="9,0" id="defaultCheck1" @if(in_array('9,0', request('filter.12', []))) checked @endif>
-    <label class="form-check-label text-secondary" for="defaultCheck1">
-        <small>9,0</small>
-    </label>
-</div>
-
-<div class="form-check">
-    <input name="filter[12][]" class="form-check-input" type="checkbox" value="7,0" id="defaultCheck1" @if(in_array('7,0', request('filter.12', []))) checked @endif>
-    <label class="form-check-label text-secondary" for="defaultCheck1">
-        <small>7,0</small>
     </label>
 </div>
 
@@ -38,16 +20,16 @@
     <small>Вага</small>
 </p>
 
-<div class="form-check">
-    <input name="filter[5][]" class="form-check-input" type="checkbox" value="45" id="defaultCheck1" @if(in_array('45', request('filter.5', []))) checked @endif>
-    <label class="form-check-label text-secondary" for="defaultCheck1">
+<div class="custom-control custom-checkbox">
+    <input name="filter[5][]" value="45" type="checkbox" class="custom-control-input" id="customCheck3" @if(in_array('45', request('filter.5', []))) checked @endif>
+    <label class="custom-control-label text-secondary" for="customCheck3">
         <small>45</small>
     </label>
 </div>
 
-<div class="form-check">
-    <input name="filter[5][]" class="form-check-input" type="checkbox" value="25" id="defaultCheck2" @if(in_array('25', request('filter.5', []))) checked @endif>
-    <label class="form-check-label text-secondary" for="defaultCheck2">
+<div class="custom-control custom-checkbox">
+    <input name="filter[5][]" value="25" type="checkbox" class="custom-control-input" id="customCheck4" @if(in_array('25', request('filter.5', []))) checked @endif>
+    <label class="custom-control-label text-secondary" for="customCheck4">
         <small>25</small>
     </label>
 </div>
