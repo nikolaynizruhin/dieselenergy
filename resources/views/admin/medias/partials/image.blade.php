cc599af79e6e288de6f6a6f0b0da505a05c55209
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-11-29 01:10:14 +0100

    Cleanup

--- resources/views/admin/medias/partials/image.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="modal fade" id="showImageModal{{ $image->id }}" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-body text-center">
-                <img src="{{ asset('storage/'.$image->path) }}" class="img-fluid" alt="{{ $image->name }}" loading="lazy">
-            </div>
-        </div>
-    </div>
-</div>
