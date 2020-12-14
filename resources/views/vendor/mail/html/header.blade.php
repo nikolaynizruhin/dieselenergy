7a5761086e405f04dc2f8c7448e21dacda15609b
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-12-14 23:14:23 +0100

    Add logo.png

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}">
-<img src="{{ asset('images/logo.svg') }}" alt="{{ config('app.name') }}" width="150">
+<img src="{{ asset('/images/logo.png') }}" alt="{{ config('app.name') }}" width="150">
 </a>
 </td>
 </tr>
