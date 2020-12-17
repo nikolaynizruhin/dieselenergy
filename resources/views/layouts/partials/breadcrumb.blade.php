063f1f401b729d2812d15344c3b476e51a9703d5
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-12-17 22:55:10 +0100

    Update breadcrumb

--- resources/views/layouts/partials/breadcrumb.blade.php
+++ resources/views/layouts/partials/breadcrumb.blade.php
@@ -1,9 +1,9 @@
-<div class="d-flex align-items-center letter-spacing text-gray-500">
+<div class="letter-spacing text-gray-500">
     <a href="{{ route('home') }}" class="text-gray-500">
         @include('layouts.partials.icon', ['name' => 'house-door-fill', 'width' => '1.2em', 'height' => '1.2em'])
     </a>
     @foreach($links as $name => $link)
-        @include('layouts.partials.icon', ['name' => 'chevron-right', 'classes' => 'mx-3', 'width' => '0.9em', 'height' => '0.9em'])
+        @include('layouts.partials.icon', ['name' => 'chevron-right', 'classes' => 'mx-2', 'width' => '0.9em', 'height' => '0.9em'])
         @if ($loop->last)
             <span class="text-gray-500">{{ $name }}</span>
         @else
