e74a78d60c0bba0eb0dbfbb8f5d40ca1a14a40d4
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2020-12-25 13:15:19 +0200

    Update nav

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -10,73 +10,73 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto d-md-none">
-                <li class="nav-item active">
+                <li class="nav-item @if (request()->routeIs('admin.dashboard')) active @endif">
                     <a class="nav-link" href="{{ route('admin.dashboard') }}">
                         @include('layouts.partials.icon', ['name' => 'pie-chart', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('dashboard.title') }} <span class="sr-only">(current)</span>
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.products.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.products.index') }}">
                         @include('layouts.partials.icon', ['name' => 'tag', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('product.plural') }}
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.orders.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.orders.index') }}">
                         @include('layouts.partials.icon', ['name' => 'bucket', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('order.plural') }}
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.customers.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.customers.index') }}">
                         @include('layouts.partials.icon', ['name' => 'people', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('customer.plural') }}
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.categories.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.categories.index') }}">
                         @include('layouts.partials.icon', ['name' => 'list-task', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('category.plural') }}
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.brands.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.brands.index') }}">
                         @include('layouts.partials.icon', ['name' => 'briefcase', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('brand.plural') }}
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.attributes.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.attributes.index') }}">
                         @include('layouts.partials.icon', ['name' => 'puzzle', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('attribute.plural') }}
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.images.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.images.index') }}">
                         @include('layouts.partials.icon', ['name' => 'image', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('common.images') }}
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.users.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.users.index') }}">
                         @include('layouts.partials.icon', ['name' => 'person-check', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('user.plural') }}
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.contacts.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.contacts.index') }}">
                         @include('layouts.partials.icon', ['name' => 'envelope', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('contact.plural') }}
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.posts.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.posts.index') }}">
                         @include('layouts.partials.icon', ['name' => 'file-text', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('post.plural') }}
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item @if (request()->routeIs('admin.currencies.*')) active @endif">
                     <a class="nav-link" href="{{ route('admin.currencies.index') }}">
                         @include('layouts.partials.icon', ['name' => 'cash-stack', 'classes' => 'mr-2', 'width' => '1.1em', 'height' => '1.1em'])
                         {{ __('currency.plural') }}
