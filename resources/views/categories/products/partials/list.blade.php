04ea7c16a45f7cc82fa66250fbaba2a431fbf8cc
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2021-01-15 01:01:30 +0100

    Add product card

--- resources/views/categories/products/partials/list.blade.php
+++ resources/views/categories/products/partials/list.blade.php
@@ -1,37 +1,5 @@
 <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
     @foreach ($products as $product)
-        <div class="col mb-4">
-            <div class="card h-100 shadow-sm">
-                <img src="{{ asset('storage/'.$product->images->first()->path) }}" class="card-img-top" alt="{{ $product->name }}" loading="lazy">
-                <div class="card-body mb-n3">
-                    <h3 class="card-title h6">{{ $product->name }}</h3>
-                    <p class="card-subtitle mb-2 text-muted">{{ $product->category->name }}</p>
-                    <p class="card-title text-primary h5">@uah($product->uah_price)</p>
-                </div>
-                @if ($product->attributes->isNotEmpty())
-                    <div class="card-body bg-light text-muted">
-                        <table class="table table-borderless m-0">
-                            <tbody>
-                                @foreach ($product->attributes as $attribute)
-                                    <tr>
-                                        <td class="p-1 letter-spacing text-gray-500 text-uppercase">
-                                            <small>{{ $attribute->name }}</small>
-                                        </td>
-                                        <td class="p-1 text-secondary">
-                                            <small>{{ $attribute->pivot->value }} {{ $attribute->measure }}</small>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                @endif
-                <div class="card-body">
-                    <a href="{{ route('products.show', $product) }}" role="button" class="btn btn-outline-secondary btn-block stretched-link">
-                        Деталі
-                    </a>
-                </div>
-            </div>
-        </div>
+        @include('categories.products.partials.product', ['product' => $product, 'hasAttributes' => true])
     @endforeach
 </div>
