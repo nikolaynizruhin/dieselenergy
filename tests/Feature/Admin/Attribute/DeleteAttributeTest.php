01b8e209673edc8a07d2f723819b679914e80118
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2021-02-20 23:32:37 +0100

    Cleanup test

--- tests/Feature/Admin/Attribute/DeleteAttributeTest.php
+++ tests/Feature/Admin/Attribute/DeleteAttributeTest.php
@@ -11,12 +11,27 @@ class DeleteAttributeTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * Product.
+     *
+     * @var \App\Models\Attribute
+     */
+    private $attribute;
+
+    /**
+     * Setup.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->attribute = Attribute::factory()->create();
+    }
+
     /** @test */
     public function guest_cant_delete_attribute()
     {
-        $attribute = Attribute::factory()->create();
-
-        $this->delete(route('admin.attributes.destroy', $attribute))
+        $this->delete(route('admin.attributes.destroy', $this->attribute))
             ->assertRedirect(route('admin.login'));
     }
 
@@ -24,14 +39,13 @@ class DeleteAttributeTest extends TestCase
     public function user_can_delete_attribute()
     {
         $user = User::factory()->create();
-        $attribute = Attribute::factory()->create();
 
         $this->actingAs($user)
             ->from(route('admin.attributes.index'))
-            ->delete(route('admin.attributes.destroy', $attribute))
+            ->delete(route('admin.attributes.destroy', $this->attribute))
             ->assertRedirect(route('admin.attributes.index'))
             ->assertSessionHas('status', trans('attribute.deleted'));
 
-        $this->assertDatabaseMissing('attributes', ['id' => $attribute->id]);
+        $this->assertDatabaseMissing('attributes', ['id' => $this->attribute->id]);
     }
 }
