3b7acf1273194f76574d79341c67d52558dd8cad
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2021-05-26 22:11:26 +0200

    Cleanup

--- app/Http/Controllers/Category/ProductController.php
+++ app/Http/Controllers/Category/ProductController.php
@@ -6,7 +6,6 @@ use App\Filters\ProductFilters;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
