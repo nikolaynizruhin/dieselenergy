1a3e1460fb77d4375da0b61d80e4606e0b951bf5
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2021-08-15 23:41:52 +0200

    Add sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use App\Notifications\ResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasFactory, Filterable;
+    use HasApiTokens, HasFactory, Notifiable, Filterable;
 
     /**
      * The attributes that are mass assignable.
