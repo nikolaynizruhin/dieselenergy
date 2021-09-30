7c577a6ac05e71324314cab2ab0c87136efa289d
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2021-09-30 14:31:21 +0200

    Update dependencies

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,11 +3,12 @@
 namespace Tests;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, LazilyRefreshDatabase;
 
     /**
      * Login user.
