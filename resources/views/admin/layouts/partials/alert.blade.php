a2027446ea179fc55681043e073beb1ff9364b20
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2021-10-20 19:47:39 +0200

    Fix alert

--- resources/views/admin/layouts/partials/alert.blade.php
+++ resources/views/admin/layouts/partials/alert.blade.php
@@ -1,11 +1,7 @@
 @if (session('status'))
-    <div class="alert alert-success alert-dismissible fade show shadow-sm d-flex" role="alert">
-        <div>
-            @include('layouts.partials.icon', ['name' => 'check-circle-fill', 'classes' => 'me-3 text-teal', 'width' => '1.1em', 'height' => '1.1em'])
-        </div>
-        <div>
-            {{ session('status') }}
-        </div>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
+    <div class="alert alert-success alert-dismissible fade show shadow-sm d-flex align-items-center" role="alert">
+        @include('layouts.partials.icon', ['name' => 'check-circle-fill', 'classes' => 'bi flex-shrink-0 me-3 text-teal', 'width' => '1.4em', 'height' => '1.4em'])
+        <div>{{ session('status') }}</div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
