67227d8425c37d4c4d714a5329cad25ead9ba831
nikolaynizruhin <lgnexus4fun@gmail.com>
2021-11-21 16:41:27 +0100

    Cleanup tests

--- tests/Feature/Admin/Cart/DeleteCartTest.php
+++ tests/Feature/Admin/Cart/DeleteCartTest.php
@@ -2,42 +2,44 @@
 
 namespace Tests\Feature\Admin\Cart;
 
-use App\Models\Order;
-use App\Models\Product;
+use App\Models\Cart;
 use Tests\TestCase;
 
 class DeleteCartTest extends TestCase
 {
+    /**
+     * Cart.
+     *
+     * @var \App\Models\Cart
+     */
+    private $cart;
+
+    /**
+     * Setup.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->cart = Cart::factory()->create();
+    }
+
     /** @test */
     public function guest_cant_delete_cart()
     {
-        $product = Product::factory()->create();
-        $order = Order::factory()->create();
-
-        $order->products()->attach($product);
-
-        $id = $order->products()->find($product->id)->pivot->id;
-
-        $this->delete(route('admin.carts.destroy', $id))
+        $this->delete(route('admin.carts.destroy', $this->cart))
             ->assertRedirect(route('admin.login'));
     }
 
     /** @test */
     public function user_can_delete_cart()
     {
-        $product = Product::factory()->create();
-        $order = Order::factory()->create();
-
-        $order->products()->attach($product);
-
-        $id = $order->products()->find($product->id)->pivot->id;
-
         $this->login()
-            ->from(route('admin.orders.show', $order))
-            ->delete(route('admin.carts.destroy', $id))
-            ->assertRedirect(route('admin.orders.show', $order))
+            ->from(route('admin.orders.show', $this->cart->order))
+            ->delete(route('admin.carts.destroy', $this->cart))
+            ->assertRedirect(route('admin.orders.show', $this->cart->order))
             ->assertSessionHas('status', trans('cart.deleted'));
 
-        $this->assertDatabaseMissing('order_product', ['id' => $id]);
+        $this->assertDeleted($this->cart);
     }
 }
