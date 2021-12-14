e2c3a067b5aa522c7ad6abce907188818e66053e
nikolaynizruhin <lgnexus4fun@gmail.com>
2021-12-15 00:46:21 +0100

    Use string markdown

--- config/app.php
+++ config/app.php
@@ -217,7 +217,6 @@ return [
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
-        'Markdown' => Illuminate\Mail\Markdown::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
