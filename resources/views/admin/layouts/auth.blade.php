3af3bfa096ee5009a423064328ec31baa364c7df
nikolaynizruhin <lgnexus4fun@gmail.com>
2022-07-16 12:24:59 +0200

    Update laravel

--- resources/views/admin/layouts/auth.blade.php
+++ resources/views/admin/layouts/auth.blade.php
@@ -9,8 +9,8 @@
 
     <title>{{ config('app.name') }}</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+    <!-- Scripts & Styles -->
+    @vite(['resources/sass/admin/admin.scss', 'resources/js/app.js'])
 </head>
 <body>
     <main class="py-5">
@@ -33,8 +33,5 @@
             </div>
         </div>
     </main>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
