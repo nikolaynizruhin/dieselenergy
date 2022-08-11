f77b8586de82712681db6b1c1282c472b19b61e1
nikolaynizruhin <lgnexus4fun@gmail.com>
2022-08-11 22:30:07 +0200

    Update laravel

--- resources/js/admin/admin.js
+++ resources/js/admin/admin.js
@@ -1,4 +1,4 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
 import EasyMDE from "easymde";
 
-new EasyMDE({ element: document.querySelector('textarea.markdown') });
+new EasyMDE({ element: document.querySelector('.markdown') });
