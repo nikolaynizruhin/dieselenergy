0d1478525ec142f452cfb434a51963d7de464c0f
nikolaynizruhin <lgnexus4fun@gmail.com>
2022-08-16 22:16:13 +0200

    Cleanup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'spam.block' => \App\Http\Middleware\BlockSpam::class,
-        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
