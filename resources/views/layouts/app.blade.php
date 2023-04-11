ed54818ededdc240f48c2aaafb27031a7f63a70c
nikolaynizruhin <lgnexus4fun@gmail.com>
2023-04-11 18:00:57 +0200

    Update GTM

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PXC3D86');
+        })(window,document,'script','dataLayer','GTM-KZP3L7G');
     </script>
     <!-- End Google Tag Manager -->
 
@@ -49,7 +49,7 @@
 <body class="d-flex flex-column h-100">
     <!-- Google Tag Manager (noscript) -->
     <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PXC3D86" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZP3L7G" height="0" width="0" style="display:none;visibility:hidden"></iframe>
     </noscript>
     <!-- End Google Tag Manager (noscript) -->
 
