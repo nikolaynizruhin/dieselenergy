d66b6b2521f235f0eea12515999d6700b532db77
nikolaynizruhin <lgnexus4fun@gmail.com>
2023-05-27 15:27:43 +0200

    Cleanup

--- app/Http/Controllers/Admin/User/PasswordController.php
+++ app/Http/Controllers/Admin/User/PasswordController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class PasswordController extends Controller
 {
