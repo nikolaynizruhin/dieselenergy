1edb368e25afc01efc7e8061fd39370f058039e4
nikolaynizruhin <lgnexus4fun@gmail.com>
2023-12-25 19:26:04 +0100

    Cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::view('/', 'home')->name('home');
 Route::view('/privacy', 'privacy')->name('privacy');
 Route::get('/sitemap.xml', SitemapController::class)->name('sitemap');
 Route::resource('contacts', ContactController::class)->only('store');
-Route::resource('carts', CartController::class);
+Route::resource('carts', CartController::class)->except('create', 'show', 'edit');
 Route::resource('orders', OrderController::class)->only(['store', 'show']);
 Route::get('shop/{category:slug}', [ProductController::class, 'index'])->name('categories.products.index');
 Route::get('products/{product:slug}', [ProductController::class, 'show'])->name('products.show');
